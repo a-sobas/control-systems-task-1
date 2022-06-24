092a6d6f16c43233679be3ee17fec9b7eaaaaa67
Artur Sobas <artursobas@outlook.com>
2022-06-25 01:36:39 +0200

    -

--- solution.py
+++ solution.py
@@ -34,7 +34,6 @@ async def save_locations(locations: CurrentLocations):
     locations_encoded = jsonable_encoder(locations)
 
     timestamp = locations_encoded["timestamp"]
-    timestamp = correct_timestamp_format(timestamp)
 
     data_to_store_str = ""
     for loc in locations_encoded["currentLocations"]:
@@ -48,11 +47,3 @@ async def save_locations(locations: CurrentLocations):
 
     with open(csv_path, "a") as f:
         f.write(data_to_store_str)
-
-
-def correct_timestamp_format(timestamp):
-    dt_in_format = "%Y-%m-%dT%H:%M:%S"
-    dt_out_format = "%Y-%m-%d %H:%M:%S"
-    corrected_timestamp = datetime.strptime(timestamp, dt_in_format)
-    
-    return corrected_timestamp.strftime(dt_out_format)
--- test.py
+++ test.py
@@ -1,29 +1,25 @@
 import requests
+from datetime import datetime
+import numpy as np
+import time
 
-BASE = "http://127.0.0.1:8000/"
 
-data = {
-    "timestamp": "2022/06/10 08:00:00",
-    "currentLocations": [
-    {
-        "id": "1",
-        "lat": 52.1,
-        "lng": 16.4
-    },
-    {
-        "id": "2",
-        "lat": 52.2,
-        "lng": 17.8
-    },
-    {
-        "id": "3",
-        "lat": 52.2,
-        "lng": 17.8
+while True:
+    id_max = np.random.randint(1, 5)
+
+    data = {
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "currentLocations": [
+            {
+                "id": str(i),
+                "lat": np.random.uniform(-90, 90),
+                "lng": np.random.uniform(-180, 180)
+            } for i in range(1, id_max + 1)
+        ]
     }
-    ]
-}
 
+    response = requests.post("http://127.0.0.1:8000/", json=data)
 
-r = response = requests.post(BASE, json=data)
+    print("Request response status code:", response.status_code)
 
-print(r.status_code)
+    time.sleep(10)
